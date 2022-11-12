c2d46317d2170c58ac3cbbb6c23059e9c113b8f1
Marat <davletshinm@list.ru>
2022-11-12 18:32:52 +0100

    add db

--- app/homework/models.py
+++ app/homework/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class HomeWork(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
