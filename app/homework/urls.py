d6bedabaa707721278cae16167af0a521889353e
Marat <davletshinm@list.ru>
2022-11-13 10:52:06 +0100

    add list view detail view

--- app/homework/urls.py
+++ app/homework/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("home/", views.home, name='index'),
     path("about/", views.About.as_view()),
     path("ip/", views.get_client_ip, name='ip'),
+    path("homework/", views.HomeWorkListView.as_view(), name='homework_list'),
+    path("homework/<int:pk>", views.HomeWorkDetailView.as_view(), name='homework-detail'),
 ]
--- app/homework/views.py
+++ app/homework/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
+from django.views import generic
 from django.views.generic import TemplateView
 from .models import HomeWork  # from homework.models
+
 
 
 def home(request, *args, **kwargs):
     m_list = HomeWork.objects.all()
     # my_list = ["eins", "zwei", "drei"]
-    return render(request, 'html/home.html', {"name_von_my_list": m_list})
+    return render(request, 'home.html', {"name_von_my_list": m_list})
 
 
 class About(TemplateView):
-    template_name = 'html/about.html'
+    template_name = 'about.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -27,4 +29,16 @@ def get_client_ip(request):
     ip = request.META.get('REMOTE_ADDR')  # REMOTE_ADDR – The IP address of the client.
     host = request.META.get('HTTP_HOST')  # HTTP_HOST – The HTTP Host header sent by the client.
     # (url=f'http://ip-api.com/json/{ip}')
-    return render(request, 'html/ip.html', {'ip_address': ip, 'host': host})  # with variables
+    return render(request, 'ip.html', {'ip_address': ip, 'host': host})  # with variables
+
+
+class HomeWorkListView(generic.ListView):
+    model = HomeWork
+    template_name = "homework_list.html"
+    context_object_name = "homework_list"  # rename
+    queryset = HomeWork.objects.all()[:5]  # first 5
+
+
+class HomeWorkDetailView(generic.DetailView):
+    model = HomeWork
+    # template_name = "homework_detail.html"  # /templates/homework/homework_detail.html
