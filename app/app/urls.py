8a7fe84c000c80524fd691d389a882ec80256583
Marat <davletshinm@list.ru>
2022-11-15 13:22:05 +0100

    add sing up

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from profiles.views import UserFormView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homework.urls')),
+    path('profiles/register/', UserFormView.as_view()),
 ]
--- app/profiles/models.py
+++ app/profiles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+# python manage.py makemigrations
+# python manage.py migrate
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+    # birthday = models.DateField()
